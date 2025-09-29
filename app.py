66e24d4ac7ecdf51b9cb596cfdfc322eaff020ad
Nikhil-Srimanth <127603231+Nikhil-Srimanth@users.noreply.github.com>
2025-09-29 14:19:03 +0530

    Model Moved

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask,render_template,redirect,url_for
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from flask import Flask,render_template,redirect,url_for,request
+
+
 
 app = Flask(__name__)
 
+try:
+    model_path = "./final_model_test"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
+    qg_pipeline = pipeline(
+        "text2text-generation",
+        model=model,
+        tokenizer=tokenizer
+    )
+except Exception as e:
+    print("Model not found")
+
 @app.route('/')
 def main():
+    
     return render_template("index.html")
-@app.route('/Ques')
-def Ques():
-    a = 
+@app.route('/gen',methods=["POST","GET"])
+def gen():
+    passage = request.form['Paragraph']
+    result = qg_pipeline(
+        "generate question: " + passage,
+        max_new_tokens=64,
+        do_sample=False
+    )
+    
+    return render_template("index.html",Quess=result)
 if __name__ == "__main__":
     app.run(debug=True)
